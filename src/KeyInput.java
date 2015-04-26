6710ea1e9c01b016cdeda81a312f74a5d3470ba8
Brendon Veronese <bsver1@hotmail.com>
2015-04-27 02:22:21 +1000

    Updates movement speed

--- src/KeyInput.java
+++ src/KeyInput.java
@@ -21,7 +21,7 @@ public class KeyInput extends KeyAdapter{
 					tempObject.setJumping(true);
 					tempObject.setVelY( -10 );
 				}
-				if (key==KeyEvent.VK_A) tempObject.setVelX( -5 );
+				if (key==KeyEvent.VK_A) tempObject.setVelX( -20 );
 				//if (key==KeyEvent.VK_S) tempObject.setVelY( 5 );
 				if (key==KeyEvent.VK_D) tempObject.setVelX( 5 );
 				
