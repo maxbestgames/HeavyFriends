b730f62e91399d37899d8bf02535af95136cb5a1
Brendon Veronese <bsver1@hotmail.com>
2015-04-27 22:37:02 +1000

    things

--- src/core/gameobjects/Player.java
+++ src/core/gameobjects/Player.java
@@ -33,8 +33,8 @@ public class Player extends TickingGameObject{
 		if(falling || jumping) {
 			velY += gravity;
 		}
-		x = Game.clamp(x, 0, Window.visibleScreenX-playerWidth);
-		y = Game.clamp(y, 0, Window.visibleScreenY-playerHeight);
+		//x = Game.clamp(x, 0, Window.visibleScreenX-playerWidth);
+		//y = Game.clamp(y, 0, Window.visibleScreenY-playerHeight);
 		
 		collision();
 	}
