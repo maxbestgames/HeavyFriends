8b6850f7444359768179fa94ca7f1c3d925ec75c
Max Best <maxbestgames@gmail.com>
2015-04-27 20:15:17 +1000

    camera and img lloader fix

--- src/BufferedImageLoader.java
+++ src/BufferedImageLoader.java
@@ -1,4 +1,39 @@
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+
 
 public class BufferedImageLoader {
+	
+	private BufferedImage image;
+	
+	public BufferedImage loadImage(String path) {
+		
+		try{
+			image = ImageIO.read(getClass().getResource(path));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return image;
+	}
 
 }
+/*private void LoadImageLevel(BufferedImage Image) {
+	int w image.getWidth();
+	int h image.getHeight();
+	
+	System.out.println("width, height" + w ", " + h);
+	
+	for(int i = 0; i<h; i++){
+		for( int j = 0; j<w; j++){
+			int pixel = image.getRGB(i, j);
+			int red = (pixel >> 16) & 0xff;
+			int green = (pixel >> 8 ) & 0xff;
+			int blue = (pixel) & 0xff;
+			
+			if(red == 255 && green == 255 & blue == 255) handler.addObject(new Block(i*32, j*32, ObjectId.Block));
+			
+		}
+	}
+}*/
--- src/Camera.java
+++ src/Camera.java
@@ -1,4 +1,32 @@
+import core.Game;
+
 
 public class Camera {
-
+	
+	private float x, y;
+	
+	public Camera(float x, float y) {
+		this.x = x;
+		this.y = y;
+	}
+	public void tick(GameObject player) {
+		x = -player.getX() + Game.WIDTH/2;
+	}
+	
+	public void setX(float x) {
+		this.x = x
+	}
+	
+	public void setY(float y) {
+		this.y = y;
+	}
+	
+	public float getX() {
+		return x;
+	}
+	
+	public float getY() {
+		return y;
+	}
+	
 }
