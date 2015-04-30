4e99fcf7080a41688ddbbace721be7602467166d
maxbestgames <maxbestgames@gmail.com>
2015-04-30 18:55:46 +1000

    more changes

--- src/core/gameobjects/Enemy.java
+++ src/core/gameobjects/Enemy.java
@@ -1,13 +1,23 @@
 package core.gameobjects;
 
+import core.enums.EnemyType;
 import core.enums.EntityID;
+import core.gameobjects.ai.EnemyAI;
+import core.handlers.EnemyBoundsHandler;
+import core.handlers.EnemyCollisionHandler;
+import core.handlers.EnemyMovementHandler;
 
 public abstract class Enemy extends TickingGameObject {
 
-	public Enemy(int x, int y, EntityID id) {
-		super(x, y, id);
-		
-	}
-
+	EnemyCollisionHandler col;
+	EnemyMovementHandler mov;
+	EnemyBoundsHandler bound;
+	EnemyAI ai;
+	EnemyType type;
 	
+	
+	public Enemy(int x, int y, EntityID id, EnemyType type) {
+		super(x, y, id);
+		this.type = type;
+	}
 }
